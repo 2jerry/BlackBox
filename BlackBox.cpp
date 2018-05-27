4a33b8c1be8f8cad46639e81db916ed32bbe4e2a
2jerry <wodml9412@gmail.com>
2018-05-27 14:17:19 +0900

    class 분리

--- BlackBox.cpp
+++ BlackBox.cpp
@@ -1,71 +1,75 @@
 #include "BlackBox.hpp"
 
-/* 구현 목록
- * 1. 파일 load
- * 2. 이미지 처리의 구간 나누기 (ROI(Region Of Interesting) 설정)
- * 3. contour 추출
- * 4. 번호판 좌표 추출
- * 5. 번호판의 조건 설정
-*/
-BlackBox::BlackBox(string fname, int _format)
+// 구현 목록
+/* 1. 파일 load
+* 2. 사각형 그리기(원하는 좌표, 크기)
+* 3. 번호판의 조건 설정
+* 4. 번호판 좌표 추출
+* 5. contour 추출*/
+
+// 1. 파일 load 
+/* _fname : 파일 이름
+* _format : 파일 형식 (VIDEO or IMAGE) */
+BlackBox::BlackBox(string _fname, int _format)
 {
+	Mat temp;
 	if (_format == VIDEO)
 	{
-		VideoCapture _cap(fname);
-		Mat temp;
+		VideoCapture _cap(_fname);
 		cap = _cap;
 
-		if (!cap.isOpened())
+		if (!_cap.isOpened())
 		{
 			cout << "Error opening video stream or file" << endl;
 			return;
 		}
 
-		cap >> temp;
-		//resize(temp, temp, Size(), 0.5, 0.5);
-		videoSize = temp.size();
+		_cap >> frame;
+		videoSize = frame.size();
 		cout << "width : " << videoSize.width << ", height : " << videoSize.height << endl;
 	}
 	else
 	{
-
-		frame = imread(fname, IMREAD_UNCHANGED);
+		frame = imread(_fname, IMREAD_UNCHANGED);
 		videoSize = frame.size();
 	}
-
+	p = new ImageProcess(frame, videoSize); // frame은 그레이
 }
 
-BlackBox::~BlackBox()
-{
-}
-
+// 2. 사각형 그리기(원하는 좌표, 크기) 
+/* _frame :  사각형을 그릴 영상
+* _xy    : 사각형을 그릴 시작 좌표
+* _size  : 사각형의 가로 세로 길이 */
 void BlackBox::drawRect(Mat& _frame, Size _xy, Size _size)
 {
 	rectangle(_frame, Rect(_xy.width, _xy.height, \
 		_size.height, _size.width), Scalar(0, 0, 255), 2);
-
 }
 
-// 2. 이미지 처리의 구간 나누기(ROI 설정)
-void BlackBox::ROI2Threshold(Mat &_image, Point _xy, Size _size, int _mode)
+// 3. 번호판의 조건 설정
+/* 1) 사각형의 면적의 조건 설정
+*  2) 차량번호판에 맞는 비율 설정
+*  3) 번호판의 높이는 0이 될 수 없음
+*  _point : 사각형의 시작,끝 점의 좌표 */
+bool BlackBox::numberDetect(Size* _point)
 {
-	Mat imageROI = _image(Rect(_xy, _size)); // ROI
-	if (_mode == 0)
-		threshold(imageROI, imageROI, 0, 0, THRESH_BINARY);
-	else
-		adaptiveThreshold(imageROI, imageROI, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY, 11, 9.0);
+	int height = _point[1].height - _point[0].height;
+	int width = _point[1].width - _point[0].width;
+	//bool flag = true;
+
+	if (height * width > 1000) // 1번 조건
+		return false;
+	if (height == 0) // 3번 조건
+		return false;
+	if ((width / height < 3) || (width / height >6)) // 2번 조건
+		return false;
+
+	return true;
 }
 
-
-// 3. contour 추출(사각형만)
-void findRectangle()
-{
-	vector<vector<Point>> contours; // 윤곽선 부분을 넣을 배열
-	vector<Vec4i> hierarchy; // 윤곽선 고유번호
-}
-
-// 4. 번호판 좌표 추출
-// 백터의 0은 시작점, 1은 끝점
+// 4. 번호판 좌표 추출 => 사각형의 시작점과 끝점을 찾아준다.
+/* point 0은 시작점, 1은 끝점
+* _poly : 추출한 4개 꼭지점의 좌표 백터 */
 Size* BlackBox::findRectPoint(vector<Point> _poly)
 {
 	Size point[2];
@@ -87,45 +91,55 @@ Size* BlackBox::findRectPoint(vector<Point> _poly)
 	return point;
 }
 
-// 5. 번호판의 조건 설정
-/* 1) 사각형의 면적의 조건 설정
- * 2) 차량번호판에 맞는 비율 설정
- * 3) 번호판의 높이는 0이 될 수 없음 */
-bool BlackBox::numberDetect(Size* _point)
+/* 5. contour 추출 (컨투어를 찾고, 번호판 확률에 사각형을 그린다)
+* _frame : 컨투어를 찾을 영상 */
+void BlackBox::detectRectangle(Mat& _frame)
 {
-	int height = _point[1].height - _point[0].height;
-	int width = _point[1].width - _point[0].width;
-	//bool flag = true;
+	vector<vector<Point>> contours; // 윤곽선 부분을 넣을 배열
+	vector<Vec4i> hierarchy; // 윤곽선 고유번호
 
-	if (height * width > 1000) // 1번 조건
-		return false;
-	if (height == 0) // 3번 조건
-		return false;
-	if ((width / height < 3) || (width / height >6)) // 2번 조건
-		return false;
+	findContours(_frame, contours, hierarchy, \
+		CV_RETR_TREE, CV_CHAIN_APPROX_NONE);
 
-	return true;
+	vector<Point> poly;
+	for (int i = 0; i < contours.size(); i++)
+	{
+		if (contours[i].size() < 100) continue;
+		approxPolyDP(contours[i], poly, 7, true);
+		if (poly.size() == 4)
+		{
+			Size* point = new Size[2];
+			point = findRectPoint(poly); // 사각형의 두 개 좌표
+
+			if (numberDetect(point)) // 번호판에 맞는 조건이면 사각형 표시
+				drawRect(frame, point[0], Size(point[1].height - point[0].height, point[1].width - point[0].width));
+			// 사각형은 크기에 맞게 그린다.
+		}
+	}
+	imshow("Frame", frame); // 이미지 출력
 }
 
-
-void BlackBox::processedVideo(int _mode)
+void BlackBox::playVideo(int _mode)
 {
-	Mat gray_img;
-	Mat imageROI, imageROI2;
+	Mat temp_frame;
 	while (1)
 	{
 		cap >> frame;
 		if (frame.empty())	break;
 
-		//if (!_mode) // gray 일때만
-		//bgr2Gray(frame);
-		processedImg();
+		cvtColor(frame, frame, COLOR_BGR2GRAY);
+		temp_frame = frame.clone(); // 이미지처리용 frame에 저장
 
+		p->startProcessing(temp_frame); // 이미지 처리 시작
 
+		cvtColor(frame, frame, COLOR_GRAY2BGR);
+		detectRectangle(temp_frame); // 사각형 그린다.
+
+									 //waitKey(0);
 		char c = (char)waitKey(1);
 		if (c == 27) break;
 	}
+	delete p; // 객체 p 정리
 	cap.release();
 	destroyAllWindows();
 }
-
--- BlackBox.hpp
+++ BlackBox.hpp
@@ -1,16 +1,10 @@
 #pragma once
-#include <opencv2\opencv.hpp>
-#include <iostream>
-#include<algorithm>
-
-using namespace std;
-using namespace cv;
+#include "ImageProcess.hpp"
 
 #define GRAY 0
 #define COLOR 1
 #define VIDEO 2
 #define IMAGE 3
-
 
 class BlackBox
 {
@@ -18,73 +12,16 @@ private:
 	Mat frame;
 	VideoCapture cap;
 	Size videoSize;
+	ImageProcess *p;
 
 public:
-	BlackBox(string fname,int _format);
-	BlackBox()	{	}// 기본 생성자
-	~BlackBox();
+	BlackBox(string fname, int _format);
+	BlackBox() { /* 기본 생성자 */ }
+	~BlackBox() { /* 소멸자 */ }
 
-	void ROI2Threshold(Mat &_image, Point _xy, Size _size, int _mode);
-	void drawRect(Mat& _frame, Size _xy,Size _size);
-	void processedVideo(int _mode);
+	void drawRect(Mat& _frame, Size _xy, Size _size);
+	void playVideo(int _mode);
 	bool numberDetect(Size* _point);
 	Size* findRectPoint(vector<Point> _poly);
-	void processedImg()
-	{
-		/*---------- Image Procepoint[0]ing -----------*/
-		cvtColor(frame, frame, COLOR_BGR2GRAY);
-		Mat blurImg;
-		blur(frame, blurImg, Size(2, 2)); // 윤곽
-		
-		ROI2Threshold(blurImg, Point(0, videoSize.height / 2.2), Size(videoSize.width, videoSize.height / 2.2),1); // 관심영역 설정 (도로)
-		ROI2Threshold(blurImg, Point(0, 0), Size(videoSize.width, videoSize.height / 2.2), 0);  // 관심영역 설정 (하늘)
-		ROI2Threshold(blurImg, Point(0, videoSize.height / 1.3), Size(videoSize.width, videoSize.height - videoSize.height / 1.3), 0); // 관심영역 설정 (자동차)
-		
-		// 커널값 클수록 흰 영역 많아짐.																															   // 커널값 클수록 흰 영역 많아짐.
-		Mat morphImg;
-		morphologyEx(blurImg, morphImg, MORPH_TOPHAT, Mat(9, 9, CV_8U));
-				
-		imshow("blurImg", blurImg);
-		imshow("morphImg", morphImg);
-
-
-		//*---------- Find Contour ----------*/
-		vector<vector<Point>> contours; // 윤곽선 부분을 넣을 배열
-		vector<Vec4i> hierarchy; // 윤곽선 고유번호
-
-		findContours(morphImg, contours, hierarchy, \
-			CV_RETR_TREE, CV_CHAIN_APPROX_NONE);
-
-		cvtColor(frame, frame, COLOR_GRAY2BGR);
-		//cvtColor(morphImg, morphImg, COLOR_GRAY2BGR);
-
-
-		vector<Point> poly;
-		for (int i = 0; i < contours.size(); i++)
-		{
-			if (contours[i].size() < 100) continue;
-			//if (contourArea(contours[i]) < 300 || contourArea(contours[i]) > 1500) continue;
-			approxPolyDP(contours[i], poly, 7, true);
-			if (poly.size() == 4)
-			{
-				// 원점이랑 제일 가까운애 찾기
-				Size* point = new Size[2];
-				point = findRectPoint(poly);
-				
-				if(numberDetect(point))
-				{
-					drawRect(frame, point[0], Size(point[1].height - point[0].height, point[1].width - point[0].width));
-					//cout << "가로 세로 비율 " << (point[1].width - point[0].width) / (point[1].height - point[0].height) << endl;
-				}
-
-				imshow("Frame", frame);
-				//waitKey(0);
-
-			}
-		}
-
-	}
-	
-	
+	void detectRectangle(Mat& _frame);
 };
-
